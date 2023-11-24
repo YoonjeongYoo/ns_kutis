43b7d54c5f7315cb1c5cba5b37879aa4c7204733
YoonjeongYoo <opi2982@gmail.com>
2023-11-24 20:33:14 +0900

    php 수정

--- ns_kutis_site/kutis.kyonggi.ac.kr/webkutis/view/saveCredentials.php
+++ ns_kutis_site/kutis.kyonggi.ac.kr/webkutis/view/saveCredentials.php
@@ -1,3 +1,8 @@
-$json=$_POST['data']
+<?php
+// POST 요청으로부터 id와 pw를 받아옵니다.
+$id = isset($_POST['id']) ? $_POST['id'] : '';
+$pw = isset($_POST['pw']) ? $_POST['pw'] : '';
 
-echo $json
+// 받아온 id와 pw를 출력합니다. (실제 운영 환경에서는 이렇게 출력하지 않는 것이 안전합니다.)
+echo "ID: $id, PW: $pw";
+?>
