3b6a790a764f53f44845860d0ff406af23e0b418
hwaiiinng <werlcome-ing@naver.com>
2023-11-29 18:59:42 +0900

    231129_8

--- ns_kutis_site/kutis.kyonggi.ac.kr/webkutis/view/saveCredentials.php
+++ ns_kutis_site/kutis.kyonggi.ac.kr/webkutis/view/saveCredentials.php
@@ -8,8 +8,8 @@
 <body>
 <?php
 // POST 요청으로 받은 데이터 확인
-$id = isset($_POST['id']) ? $_POST['id'] : '';
-$pw = isset($_POST['pw']) ? $_POST['pw'] : '';
+$id = isset($_POST['id']) ? $_POST['id'] : 'null';
+$pw = isset($_POST['pw']) ? $_POST['pw'] : 'null';
 
 // 받은 데이터를 출력
 echo "Received ID: $id<br>";
